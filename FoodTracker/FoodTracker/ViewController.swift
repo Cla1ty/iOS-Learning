b20cbd14e5bf455f656e1aa85eb5e1aaef199af8
Dwi Ariyanto <dwi.ariyanto@jakmall.com>
2019-09-11 13:36:59 +0700

    feat: init food tracker

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jakmall on 11/09/19.
+//  Copyright © 2019 Kristal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
